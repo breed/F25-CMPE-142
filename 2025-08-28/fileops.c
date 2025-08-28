6bb79d7263147ca596d4d93c5e93d8b9985f8aaf
ben reed <bcr33d@gmail.com>
2025-08-28 12:00:15 -0700

    end of class

--- 2025-08-28/fileops.c
+++ 2025-08-28/fileops.c
@@ -1,5 +1,31 @@
 #include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+struct s {
+    int i;
+    int j;
+};
 
 int main() {
-    write(1, "hello!\n", 7);
+    struct s mys = { 1,2};
+    struct s *pmys = &mys;
+    int myi = (*pmys).i;
+    int myotheri = pmys->i;
+
+    char *chars = "1234";
+    char c = chars[1];
+    //char c = *(chars+1)
+    //char c = *(1+chars)
+    //char c = 1[chars];
+
+    int i = 0x006e6562;
+    char *ptr = (char*)&i;
+    printf("%s\n", ptr);
+    int rc = write(4, "hello!\n", 7);
+    if (rc == -1) {
+        perror("problem with 4");
+    }
+    exit(2);
 }
