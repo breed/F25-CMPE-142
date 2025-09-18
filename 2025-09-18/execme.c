2c446a5a43bb63c8d9bc9d998cddb1910b00bedb
ben reed <bcr33d@gmail.com>
2025-09-18 12:37:11 -0700

    final 9/18 changes to execme.c

--- 2025-09-18/execme.c
+++ 2025-09-18/execme.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char **argv) {
-    execlp("echo", "echo", "hello world!", NULL);
-    printf("all done\n");
+    if (fork() == 0) {
+        execlp("echo", "echo-no", "5309", NULL);
+        exit(99);
+    }
+    int status;
+    wait(&status);
+    printf("all done exit code %d\n", WEXITSTATUS(status));
 }
