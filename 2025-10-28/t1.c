d98b1b054fe33fcb3420e03a25cd6f96d2ca1f82
ben reed <bcr33d@gmail.com>
2025-10-28 10:16:29 -0700

    in class 2025-10-23

---
+++ 2025-10-28/t1.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <pthread.h>
+// for atomic_exchange
+#include <stdatomic.h>
+
+static volatile int counter = 0;
+
+static volatile int locked = 0;
+
+void lock() {
+    while (locked);
+    locked = 1;
+}
+
+void unlock() {
+    locked = 0;
+}
+
+// mythread()
+//
+// Simply adds 1 to counter repeatedly, in a loop
+// No, this is not how you would add 10,000,000 to
+// a counter, but it shows the problem nicely.
+//
+void *mythread(void *arg) {
+    printf("%s: begin\n", (char *) arg);
+    int i;
+    for (i = 0; i < 1e7; i++) {
+        lock();
+        counter++;
+        unlock();
+    }
+    printf("%s: done\n", (char *) arg);
+    return NULL;
+}
+
+// main()
+//
+// Just launches two threads (pthread_create)
+// and then waits for them (pthread_join)
+//
+int main(int argc, char *argv[]) {
+    pthread_t p1, p2;
+    printf("main: begin (counter = %d)\n", counter);
+#if 0
+    mythread("A");
+    mythread("B");
+#else
+    pthread_create(&p1, NULL, mythread, "A");
+    pthread_create(&p2, NULL, mythread, "B");
+
+    // join waits for the threads to finish
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
+#endif
+    printf("main: done with both (counter = %d)\n", counter);
+    return 0;
+}
